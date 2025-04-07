c93901aa5ead74d8c9e561db70fd9f1e5324ccbf
HanJunGeum <84554709+HanJun-g0id@users.noreply.github.com>
2025-04-07 09:57:56 +0900

    ProfileCard.jsx

---
+++ src/Card/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+function ProfileCard(props) {
+    return (
+        <Card title="HanJun-Geum" backgroundColor="#0d81e7">
+            <p>안녕하세요, 한준입니다.</p>
+            <p>드디어 끝나간다는게 너무나 신납니다.</p>
+        </Card>
+    );
+}
+
+export default ProfileCard;
