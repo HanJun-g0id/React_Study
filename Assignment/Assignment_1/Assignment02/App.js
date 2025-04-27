da144c7c28c0032a1a8189c6b8e4fae8cc5be241
HanJunGeum <84554709+HanJun-g0id@users.noreply.github.com>
2025-04-27 21:48:53 +0900

    Feat: 전체적인 레이아웃 및 배경 적용

--- Assignment/Assignment_1/Assignment02/App.js
+++ Assignment/Assignment_1/Assignment02/App.js
@@ -1,1 +1,15 @@
-test
+import React from 'react';
+import TodoApp from './TodoApp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gradient-background">
+      <div className="todo-wrapper fixed-box">
+        <TodoApp />
+      </div>
+    </div>
+  );
+}
+
+export default App;
