1d7a437bf2f63ffde932b163c27931e2d92ea509
HanJunGeum <84554709+HanJun-g0id@users.noreply.github.com>
2025-05-03 21:39:51 +0900

    Fear: 화면 하단 고정 문구 구현

---
+++ Assignment/ImitateSite/src/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      Dev Race @ 2024, All rights reserved
+    </footer>
+  );
+}
+
+export default Footer;
