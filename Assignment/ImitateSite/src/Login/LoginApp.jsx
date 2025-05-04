38a9cdd07942162c799f2243fc2b3db2ae92fda7
HanJunGeum <84554709+HanJun-g0id@users.noreply.github.com>
2025-05-04 16:22:06 +0900

    feat: 각 로그인 버튼 클릭 시 회원가입 유도 모달 추가

--- Assignment/ImitateSite/src/Login/LoginApp.jsx
+++ Assignment/ImitateSite/src/Login/LoginApp.jsx
@@ -1,12 +1,23 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import SignUp from '../SignUp/MoveSignUp';
 import './LoginApp.css';
 
 function LoginApp() {
 
+  const [showSignUp, setShowSignUp] = useState(false);
+
+  const handleLoginClick = () => {
+    setShowSignUp(true);
+  };
+
+  const handleCloseSignUp = () => {
+    setShowSignUp(false);
+  };
+
   return (
     <div className="login-container">
       <h2>Log In</h2>
-      <button className="login-btn">
+      <button className="login-btn" onClick={handleLoginClick}>
         <img
           src="/GoogleLogo.png"
           alt="Google Logo"
@@ -14,7 +25,7 @@ function LoginApp() {
         />
         Log in with Google
       </button>
-      <button className="login-btn">
+      <button className="login-btn" onClick={handleLoginClick}>
         <img
           src="/GithubLogo.png"
           alt="Github Logo"
@@ -22,6 +33,7 @@ function LoginApp() {
         />
         Log in with Github
       </button>
+      {showSignUp && <SignUp onClose={handleCloseSignUp} />}
     </div>
   ); 
 }
