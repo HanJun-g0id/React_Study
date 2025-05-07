42fa9c7410afe69c251fe3ff72b247a6df58990a
HanJunGeum <84554709+HanJun-g0id@users.noreply.github.com>
2025-05-07 09:27:05 +0900

    style: 회원가입 모달 디자인 개선

--- Assignment/ImitateSite/src/SignUp/MoveSignUp.jsx
+++ Assignment/ImitateSite/src/SignUp/MoveSignUp.jsx
@@ -3,10 +3,10 @@ import './MoveSignUp.css';
 import { useNavigate } from 'react-router-dom';
 import SignUpForm from './SignUpForm';
 
+
 function SignUp({onClose}) {
   const navigate = useNavigate();
   const [showForm, setShowForm] = useState(false);
-  
 
   if (showForm) {
     return <SignUpForm onClose={onClose} />;
@@ -19,7 +19,9 @@ function SignUp({onClose}) {
           &times;
         </button>
         <div className="modal-content">
+          <img src="/exclam.png" className="modal-icon" />
           <h3>회원가입으로 이동합니다!</h3>
+          <p className="modal-desc">가입되지 않은 계정입니다.</p>
           <button className="confirm-btn" onClick={() => navigate('/signup')}>
             확인
           </button>
