71044361c6631ee6441807918d8f73ac899fbcb2
HanJunGeum <84554709+HanJun-g0id@users.noreply.github.com>
2025-05-09 17:52:32 +0900

    feat: 회원가입 최종 페이지 연결

--- Assignment/ImitateSite/src/App.jsx
+++ Assignment/ImitateSite/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginApp from './Study/Login/LoginApp';
 import SignUpForm from './Study/SignUp/SignUpForm';
+import CompleteSignUp from './Study/SignUp/CompleteSignUp';
 import Footer from './Study/Footer/Footer';
 import Header from './Study/Header/Header';
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginApp />} />
         <Route path="/signup" element={<SignUpForm />} />
+        <Route path="/complete" element={<CompleteSignUp />} />
       </Routes>
       <Footer />
     </BrowserRouter>
