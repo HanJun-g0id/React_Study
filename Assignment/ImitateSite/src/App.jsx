51ef71aa6367157b6702f9f078409fe372b72547
HanJunGeum <84554709+HanJun-g0id@users.noreply.github.com>
2025-05-10 18:58:19 +0900

    feat: 페이지 별 헤더 구분 추가

--- Assignment/ImitateSite/src/App.jsx
+++ Assignment/ImitateSite/src/App.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import LoginApp from './Study/Login/LoginApp';
 import SignUpForm from './Study/SignUp/SignUpForm';
 import CompleteSignUp from './Study/SignUp/CompleteSignUp';
 import Footer from './Study/Footer/Footer';
 import Header from './Study/Header/Header';
+import Home from './Study/Home/Home';
 
-function App() {
+function AppContent() {
+  const location = useLocation();
+  const hideHeaderPaths = ["/home"];
+  const isHeaderHidden = hideHeaderPaths.includes(location.pathname);
+
   return (
-    <BrowserRouter>
-      <Header/>
+    <>
+      {!isHeaderHidden && <Header />}
       <Routes>
         <Route path="/" element={<LoginApp />} />
         <Route path="/signup" element={<SignUpForm />} />
         <Route path="/complete" element={<CompleteSignUp />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
       <Footer />
+    </>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppContent />
     </BrowserRouter>
   );
 }
